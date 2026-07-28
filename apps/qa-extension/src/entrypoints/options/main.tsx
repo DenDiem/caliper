b847175d8fda472bb9ed41aa9e943409a4794528
Denys Baranov <36038876+DenDiem@users.noreply.github.com>
2026-07-28 13:47:33 +0300

    feat(design): shared design-system tokens + IBM Plex, wired into extension pages and overlay

--- apps/qa-extension/src/entrypoints/options/main.tsx
+++ apps/qa-extension/src/entrypoints/options/main.tsx
@@ -1,3 +1,10 @@
+import '@caliper/design/tokens.css';
+import '@fontsource/ibm-plex-sans/latin-400.css';
+import '@fontsource/ibm-plex-sans/latin-500.css';
+import '@fontsource/ibm-plex-sans/latin-600.css';
+import '@fontsource/ibm-plex-mono/latin-400.css';
+import '@fontsource/ibm-plex-mono/latin-500.css';
+import '@fontsource/ibm-plex-mono/latin-600.css';
 import {render} from 'preact';
 import {App} from './App';
 import './options.css';
--- apps/qa-extension/src/entrypoints/sidepanel/main.tsx
+++ apps/qa-extension/src/entrypoints/sidepanel/main.tsx
@@ -1,3 +1,11 @@
+import '@caliper/design/tokens.css';
+import '@fontsource/ibm-plex-sans/latin-400.css';
+import '@fontsource/ibm-plex-sans/latin-500.css';
+import '@fontsource/ibm-plex-sans/latin-600.css';
+import '@fontsource/ibm-plex-sans/latin-700.css';
+import '@fontsource/ibm-plex-mono/latin-400.css';
+import '@fontsource/ibm-plex-mono/latin-500.css';
+import '@fontsource/ibm-plex-mono/latin-600.css';
 import {render} from 'preact';
 import {App} from './App';
 import './sidepanel.css';
